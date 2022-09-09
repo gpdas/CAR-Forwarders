dc74d05c93b146c130a268c4e5880c990c812a29
Andy Perrett <43847848+18684092@users.noreply.github.com>
2022-09-09 13:40:27 +0100

    Update MQTTForward.py
    VDOP C/N0_Max

--- MQTTForward.py
+++ MQTTForward.py
@@ -132,9 +132,13 @@ class MQTTforwarder:
                 try:
                     gps['accuracy'] = float(mDict['PDOP'])
                     gps['HDOP'] = float(mDict['HDOP'])
+                    gps['VDOP'] = float(mDict['VDOP'])
+                    gps['C/N0_MAX'] = float(mDict['C/N0_MAX'])
                 except:
                     gps['accuracy'] = -1
                     gps['HDOP'] = -1
+                    gps['VDOP'] = -1
+                    gps['C/N0_MAX'] = -1
 
                 gps['row'] = ""
                 # NOTE The row isn't sent by STDv2 - it also won't be known by the device?
